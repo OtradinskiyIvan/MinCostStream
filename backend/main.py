bcb1c330e2bde6ba65b465438df90241ef70ed03
Ivan <OtradinskiyIvan>
2026-05-21 19:05:40 +0300

    fix: fixed models import

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from models import AlgorithmRequest
+from .models import AlgorithmRequest
 
 app = FastAPI(title="MCMF Solver API", version="0.1.0")
 
